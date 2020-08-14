e3f910101f18f5e9dcbe53515da68bcedb142c84
kirito-dual <0909688604ag@gmail.com>
2020-08-14 17:15:52 +0700

    delete

--- model/ChatBoxModel.go
+++
@@ -1,7 +0,0 @@
-package model
-type ChatBoxModel struct {
-	AbstractModel
-	Sender string `json:"sender"`
-	Receiver string `json:"receiver"`
-	Messages []MessageModel
-}
--- repository/ChatBoxRepo.go
+++
@@ -1,7 +0,0 @@
-package repository
-
-import "golangproject/model"
-
-type ChatBoxRepo interface {
-	GetChatBoxs() ([]model.ChatBoxModel, error)
-}
