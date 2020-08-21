9a3f64af8fca9bf2508228122a2b4a3efebcd525
Nghi L. M. Nhựt <nghinhut@gmail.com>
2020-08-21 15:48:11 +0700

    fix(check origin): skip origin checking

--- pkg/service/test-handler.go
+++ pkg/service/test-handler.go
@@ -37,6 +37,9 @@ var broker *TestBroker
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
 }
 
 type Message struct {
