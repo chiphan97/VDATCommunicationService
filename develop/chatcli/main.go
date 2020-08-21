077869ed52ba48ec4ae186b961df6fd09d9a3232
Nguyen Chi Cuong <nguyenchicuong2402@gmail.com>
2020-08-21 09:24:00 +0700

    clone cmd to develop

---
+++ develop/chatcli/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/gorilla/websocket"
+	"gitlab.com/vdat/mcsvc/chat/pkg/model"
+	"log"
+	"os"
+	"time"
+)
+
+func main() {
+	c, _, err := websocket.DefaultDialer.Dial("ws://localhost:5000/test", nil)
+	fmt.Println("Nhap nguoi gui : ")
+	senderReader := bufio.NewReader(os.Stdin)
+	senderInput, _ := senderReader.ReadString('\n')
+	senderInput = senderInput[:len(senderInput)-1]
+
+	fmt.Println("Nhap nguoi nhan : ")
+	receivererReader := bufio.NewReader(os.Stdin)
+	receivererInput, _ := receivererReader.ReadString('\n')
+	receivererInput = receivererInput[:len(receivererInput)-1]
+	if err != nil {
+		log.Fatal("dial:", err)
+	}
+	defer c.Close()
+
+	go func() {
+		for {
+			_, message, err := c.ReadMessage()
+			if err != nil {
+				log.Println("read:", err)
+				return
+			}
+			log.Printf("recv: %s", message)
+			time.Sleep(2 * time.Microsecond)
+		}
+	}()
+	for {
+		fmt.Println("message:")
+		messageReader := bufio.NewReader(os.Stdin)
+		messageInput, _ := messageReader.ReadString('\n')
+		messageInput = messageInput[:len(messageInput)-1]
+		messagePayload := model.MessagePayload{
+
+			Message:    messageInput,
+			SenderID:   senderInput,
+			ReceiverID: receivererInput,
+		}
+		if err = c.WriteJSON(messagePayload); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+}
