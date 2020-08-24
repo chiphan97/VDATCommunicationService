94c892fe53abf558b8e226f1e3ac0999ccc55cdf
Nghi L. M. Nhựt <nghinhut@gmail.com>
2020-08-24 23:31:17 +0700

    chore(monitor): add metrics

--- cmd/chatserver/main.go
+++ cmd/chatserver/main.go
@@ -3,13 +3,16 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"gitlab.com/vdat/mcsvc/chat/pkg/database"
 	"gitlab.com/vdat/mcsvc/chat/pkg/handler"
 	"gitlab.com/vdat/mcsvc/chat/pkg/model"
 	"gitlab.com/vdat/mcsvc/chat/pkg/service"
 	"golang.org/x/net/websocket"
 	"log"
+	"net"
 	"net/http"
+	"os"
 )
 
 func echoHandler(ws *websocket.Conn) {
@@ -63,6 +66,8 @@ func serveHome(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	go metrics()
+
 	database.Connect()
 	http.HandleFunc("/", serveHome)
 	http.Handle("/echo", websocket.Handler(echoHandler))
@@ -74,5 +79,20 @@ func main() {
 	if err != nil {
 		panic("Error: " + err.Error())
 	}
+}
 
+func metrics() {
+	// The debug listener mounts the http.DefaultServeMux, and serves up
+	// stuff like the Prometheus metrics route, the Go debug and profiling
+	// routes, and so on.
+	debugListener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		fmt.Println("transport", "debug/HTTP", "during", "Listen", "err", err)
+		os.Exit(1)
+	}
+
+	defer debugListener.Close()
+
+	http.DefaultServeMux.Handle("/metrics", promhttp.Handler())
+	fmt.Println(http.Serve(debugListener, http.DefaultServeMux))
 }
