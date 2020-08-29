76b6514d772ac939ea3da6ef5824c4e2d5e72dcd
ducthien <tntran496@gmail.com>
2020-08-29 11:10:38 +0700

    "update useronline"

--- pkg/service/UserOnlineService.go
+++ pkg/service/UserOnlineService.go
@@ -6,8 +6,8 @@ import (
 	"gitlab.com/vdat/mcsvc/chat/pkg/repository/impl"
 )
 
-func GetListUSerOnlineService(userHide string) ([]model.UserOnline, error) {
-	return impl.NewUserOnlineRepoImpl(database.DB).GetListUSerOnline(userHide)
+func GetListUSerOnlineService() ([]model.UserOnline, error) {
+	return impl.NewUserOnlineRepoImpl(database.DB).GetListUSerOnline()
 }
 func AddUserOnlineService(online model.UserOnline) error {
 	return impl.NewUserOnlineRepoImpl(database.DB).AddUserOnline(online)
