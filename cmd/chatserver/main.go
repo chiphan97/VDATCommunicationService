60276289683a4a3eabd074694e524791ffbdc069
Nghi L. M. Nhựt <nghinhut@gmail.com>
2020-10-19 23:26:34 +0700

    Update chatserver/main.go, listen on all interfaces

--- cmd/chatserver/main.go
+++ cmd/chatserver/main.go
@@ -104,7 +104,7 @@ func main() {
 
 	srv := &http.Server{
 		Handler: r,
-		Addr:    "127.0.0.1:5000",
+		Addr:    ":5000",
 		// Good practice: enforce timeouts for servers you create!
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
