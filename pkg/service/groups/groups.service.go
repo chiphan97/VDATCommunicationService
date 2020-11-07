915c3a690f07b86cb453dbf5e23ba1b91e1d5aed
minhkhoa <khoajunior666@gmail.com>
2020-11-07 11:07:27 +0700

    [Group]: change name group

--- pkg/service/groups/groups.service.go
+++ pkg/service/groups/groups.service.go
@@ -186,14 +186,14 @@ func getNameGroupForGroup11(dto []Dto, id string) ([]Dto, error) {
 					value, ok := userdetail.ListUserGlobal[users[j].ID]
 					if ok == true {
 						dto[i].Name = value.Username
-						if value.FirstName != "" {
+						if value.FirstName != "" || value.LastName != "" {
 							dto[i].Name = value.FirstName + " " + value.LastName
 						}
 					} else {
 						user := userdetail.GetUserFromKCById(users[j].ID)
 						fmt.Println(user)
 						dto[i].Name = user.Username
-						if user.FirstName != "" {
+						if user.FirstName != "" || value.LastName != "" {
 							dto[i].Name = user.FirstName + " " + user.LastName
 						}
 					}
